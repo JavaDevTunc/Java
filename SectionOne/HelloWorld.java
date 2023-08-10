dad815cbcabc38dbe37402e3bff736ea4e7d7ff0
JavaDevTunc <devoncutunc@gmail.com>
2023-08-10 13:12:49 +0300

    Create HelloWorld.java
    Entry point to programming.

---
+++ SectionOne/HelloWorld.java
@@ -0,0 +1,9 @@
+package SectionOne;
+
+
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("I am learning Java.");
+    }
+}
